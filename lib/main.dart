8038e28d52ad4b55934564249ad4a0d8f065c61d
punkushal <spycymagar@gmail.com>
2024-12-04 12:37:14 +0545

    Home Widget Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:student_management_ui/screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
+      home: const SplashScreen(),
     );
   }
 }
