0cdd1aa5a0961bb8f380e9d25371dd3e1b8a1971
punkushal <spycymagar@gmail.com>
2024-12-04 14:29:23 +0545

    Other category Item Added

--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:student_management_ui/widgets/custom_card.dart';
+import 'package:student_management_ui/widgets/card_content.dart';
 import 'package:student_management_ui/widgets/custom_container.dart';
 
 class CategoryList extends StatelessWidget {
@@ -27,8 +27,62 @@ class CategoryList extends StatelessWidget {
         ),
         CustomContainer(
           content: CardContent(
+            label: 'Attendence',
+            imageUrl: 'images/immigration.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Syllabus',
+            imageUrl: 'images/book.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Time Table',
+            imageUrl: 'images/study-time.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
             label: 'Assignments',
             imageUrl: 'images/assignment.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Exams',
+            imageUrl: 'images/exam.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Results',
+            imageUrl: 'images/result.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Fees',
+            imageUrl: 'images/fee.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Events',
+            imageUrl: 'images/calendar.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Inbox',
+            imageUrl: 'images/inbox.png',
+          ),
+        ),
+        CustomContainer(
+          content: CardContent(
+            label: 'Ask Doubts',
+            imageUrl: 'images/ask.png',
           ),
         ),
       ],
